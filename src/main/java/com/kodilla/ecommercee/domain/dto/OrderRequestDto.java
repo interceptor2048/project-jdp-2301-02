ebc5541aef75ee54ee52af74c9b6e525a89a687e
viepovsky <115808064+viepovsky@users.noreply.github.com>
2023-01-08 12:20:46 +0100

    Jdp230102 4 order controller (#6)
    * JDP230102-4 Order controller initial implementation
    
    * Added CrossOrigin adnotation to OrderController
    
    * Minor change in Controller path

---
+++ src/main/java/com/kodilla/ecommercee/domain/dto/OrderRequestDto.java
@@ -0,0 +1,12 @@
+package com.kodilla.ecommercee.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderRequestDto {
+    public Long cartId;
+}
