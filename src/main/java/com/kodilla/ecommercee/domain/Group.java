0d9ca4f97f2198a3cd9b89d571d217b363066d99
Oskar R <o.rajzner@gmail.com>
2023-01-10 18:38:17 +0100

    Group Entity - implementation - JDP230102-13

--- src/main/java/com/kodilla/ecommercee/domain/Group.java
+++ src/main/java/com/kodilla/ecommercee/domain/Group.java
@@ -3,8 +3,12 @@ package com.kodilla.ecommercee.domain;
 import lombok.*;
 
 import javax.persistence.*;
-
-@Entity(name = "GROUPS")
+import java.util.List;
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "PRODUCT_GROUPS")
 public class Group {
 
     @Id
@@ -12,4 +16,10 @@ public class Group {
     @Column(name = "ID", unique = true)
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @Column(name = "NAME")
+    private String name;
+
+    @OneToMany(targetEntity = Product.class, mappedBy = "group", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<Product> productList;
 }
