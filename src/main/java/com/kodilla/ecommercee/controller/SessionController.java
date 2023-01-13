5d17dc2d02307f72b30b73542dfd834a5a819c91
andwis93 <110222998+andwis93@users.noreply.github.com>
2023-01-13 12:13:13 +0100

    Update SessionController.java

--- src/main/java/com/kodilla/ecommercee/controller/SessionController.java
+++ src/main/java/com/kodilla/ecommercee/controller/SessionController.java
@@ -4,7 +4,6 @@ import com.kodilla.ecommercee.domain.dto.SessionDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import java.util.Random;
 
 @RestController
 @RequestMapping("/Session")
@@ -15,13 +14,6 @@ public class SessionController {
     public ResponseEntity<SessionDto> createSession(@PathVariable long userId) {
         SessionDto sessionDto = new SessionDto(true,2345L);
         return ResponseEntity.ok(sessionDto);
-    }
-
-    @PutMapping(value = "genUserKey/{userId}")
-    public ResponseEntity<String>genUserKey(@PathVariable long userId) {
-        Random random = new Random();
-        int userKey = random.nextInt(90000) + 10000;
-        return new ResponseEntity<>("UserKey for: " + userId + " is: " + userKey, HttpStatus.OK);
     }
 }
 
