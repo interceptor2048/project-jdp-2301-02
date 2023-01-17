fdc00ada5ea08eebad4cb1bb7c95ab5063c06701
Oskar R <o.rajzner@gmail.com>
2023-01-17 19:26:36 +0100

    OrderController - method implementation v.1.0

---
+++ src/main/java/com/kodilla/ecommercee/exception/OrderNotFoundException.java
@@ -0,0 +1,4 @@
+package com.kodilla.ecommercee.exception;
+
+public class OrderNotFoundException extends Exception {
+}
--- src/test/java/com/kodilla/ecommercee/domain/CartEntityTestSuite.java
+++ src/test/java/com/kodilla/ecommercee/domain/CartEntityTestSuite.java
@@ -1,4 +1,4 @@
-package com.kodilla.ecommercee;
+package com.kodilla.ecommercee.domain;
 
 import com.kodilla.ecommercee.domain.*;
 import com.kodilla.ecommercee.repository.CartRepository;
