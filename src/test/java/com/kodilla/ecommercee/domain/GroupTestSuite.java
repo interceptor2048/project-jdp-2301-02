6d52c390e143289688a41ffac1a888e2272f6b17
DawidNst <dawidniesterczuk@gmail.com>
2023-01-17 12:17:13 +0100

    ''

--- src/test/java/com/kodilla/ecommercee/domain/GroupTestSuite.java
+++ src/test/java/com/kodilla/ecommercee/domain/GroupTestSuite.java
@@ -14,6 +14,7 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 @Transactional
 class GroupTestSuite {
+
     @Autowired
     private GroupRepository groupRepository;
     @Autowired
