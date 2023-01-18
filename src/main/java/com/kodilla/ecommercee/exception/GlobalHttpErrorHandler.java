060aecb7924da780f2904df0525fd5a90299a6fb
andwis93 <andwis17@gmail.com>
2023-01-18 20:12:29 +0100

    JDP230102-21 Product Controller initial implementation

---
+++ src/main/java/com/kodilla/ecommercee/exception/GlobalHttpErrorHandler.java
@@ -0,0 +1,26 @@
+package com.kodilla.ecommercee.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class GlobalHttpErrorHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(EmptyFieldException.class)
+    public ResponseEntity<Object> handlerEmptyFieldException(EmptyFieldException excetpion) {
+        return new ResponseEntity<>("One or more of required fields is incorrect", HttpStatus.OK);
+    }
+
+    @ExceptionHandler(RecordNotExistsException.class)
+    public ResponseEntity<Object> handlerRecordNoExistsException(RecordNotExistsException excetpion) {
+        return new ResponseEntity<>("Object with given id doesn't exist", HttpStatus.OK);
+    }
+
+    @ExceptionHandler(RecordExistsException.class)
+    public ResponseEntity<Object> handlerRecordExistsException(RecordExistsException excetpion) {
+        return new ResponseEntity<>("Object already exist", HttpStatus.OK);
+    }
+
+}
